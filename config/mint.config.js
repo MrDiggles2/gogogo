c94e7800365dfe5be64ca23794f8bbd498751989
Chan Lee <chanlee@Chans-MacBook-Air.local>
2018-08-04 09:06:02 -0500

    adds some new configs

---
+++ config/mint.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    unit: 20,
+    spacing: 5,
+    margin: 20,
+    radiusRatio: 0.6,
+    vertical: true,
+
+    background: '#6BA292',
+    black: '#000000',
+    white: '#FFFFFF',
+    outline: 'rgba(0,0,0,0)',
+};
---
+++ config/netflix.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    unit: 20,
+    spacing: 10,
+    margin: 10,
+    radiusRatio: 0.6,
+    vertical: false,
+
+    background: '#000000',
+    black: '#505050',
+    white: '#FFFFFF',
+    outline: 'rgba(0,0,0,0)',
+};
